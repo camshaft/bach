87e32e4225ebce751713ed168f870973a14b4433
Cameron Bytheway <bytheway.cameron@gmail.com>
2025-02-28 13:07:55 -0700

    fix: branch rand scope for kani compilation (#39)

--- bach/src/rand.rs
+++ bach/src/rand.rs
@@ -1,39 +1,67 @@
-use bolero_generator::driver;
 use core::{
     future::Future,
     pin::Pin,
     task::{Context, Poll},
 };
 use pin_project_lite::pin_project;
-use rand_xoshiro::{rand_core::SeedableRng, Xoshiro256PlusPlus};
 
 pub use bolero_generator::prelude::*;
 
-pub struct Scope {
-    driver: Option<Box<driver::object::Object<driver::Rng<Xoshiro256PlusPlus>>>>,
-}
+#[cfg(not(kani))]
+mod standard {
+    use bolero_generator::driver;
+    use rand_xoshiro::{rand_core::SeedableRng, Xoshiro256PlusPlus};
 
-impl Scope {
-    pub fn new(seed: u64) -> Self {
-        let rng = Xoshiro256PlusPlus::seed_from_u64(seed);
-        let driver = driver::Rng::new(rng, &Default::default());
-        let driver = driver::object::Object(driver);
-        let driver = Box::new(driver);
-        Self {
-            driver: Some(driver),
+    pub struct Scope {
+        driver: Option<Box<driver::object::Object<driver::Rng<Xoshiro256PlusPlus>>>>,
+    }
+
+    impl Scope {
+        pub fn new(seed: u64) -> Self {
+            let rng = Xoshiro256PlusPlus::seed_from_u64(seed);
+            let driver = driver::Rng::new(rng, &Default::default());
+            let driver = driver::object::Object(driver);
+            let driver = Box::new(driver);
+            Self {
+                driver: Some(driver),
+            }
+        }
+
+        pub fn enter<F: FnOnce() -> O, O>(&mut self, f: F) -> O {
+            let Some(driver) = self.driver.take() else {
+                // the task likely panicked so just execute the function without the random scope
+                return f();
+            };
+            let (driver, res) = bolero_generator::any::scope::with(driver, f);
+            self.driver = Some(driver);
+            res
         }
     }
+}
 
-    pub fn enter<F: FnOnce() -> O, O>(&mut self, f: F) -> O {
-        let Some(driver) = self.driver.take() else {
-            // the task likely panicked so just execute the function without the random scope
-            return f();
-        };
-        let (driver, res) = bolero_generator::any::scope::with(driver, f);
-        self.driver = Some(driver);
-        res
+/// Ensure compatibility with kani
+#[cfg(any(kani, test))]
+mod kani_impl {
+    #![cfg_attr(test, allow(dead_code))]
+
+    pub struct Scope(());
+
+    impl Scope {
+        pub fn new(seed: u64) -> Self {
+            let _ = seed;
+            Self(())
+        }
+
+        pub fn enter<F: FnOnce() -> O, O>(&mut self, f: F) -> O {
+            f()
+        }
     }
 }
+
+#[cfg(kani)]
+pub use kani_impl::Scope;
+#[cfg(not(kani))]
+pub use standard::Scope;
 
 impl From<u64> for Scope {
     fn from(value: u64) -> Self {
